75d7caefe7f6747de7df0bf9addb0f81e8c21a0b
Bernardroid <bernardogaribaybravo@gmail.com>
2019-03-08 22:51:27 -0600

    Add files via upload

---
+++ contentbot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var auth = require('./auth.json');
+
+client.login(auth.token);
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+var artChannel = "551186283314151424";
+
+client.on('message', msg => {
+
+	if(msg.channel.id != artChannel)
+	{
+		//console.log(msg.channel.id);
+		//console.log(artChannel);
+		return;
+	}
+	
+	if(msg.attachments.size > 0 || msg.embeds.length > 0)
+	{
+		//Do nothing
+	}
+	else
+	{
+		//msg.channel.send('imma delete this');
+		msg.delete();
+	}
+
+}, 100);
+
